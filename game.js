896c8f510f7e0c7168dff5485ed834d7ab96ee55
basuabhirup <basu.abhirup@yahoo.com>
2021-07-13 17:09:46 +0530

    Added the handler function against the click event on any of the buttons

--- game.js
+++ game.js
@@ -1,8 +1,11 @@
-// Creating an empty array:
-gamePattern = [];
+// Creating an empty array to store the randomly generated game pattern:
+var gamePattern = [];
 
 // Creating an array to hold the button colors:
 var buttonColours = ["red","blue","green","yellow"];
+
+// Creating an empty array to store the user clicked pattern:
+var userClickedPattern = [];
 
 // Defining a function to select a random button + animate the button + play sound:
 function nextSequence() {
@@ -13,3 +16,13 @@ function nextSequence() {
   var selectionSound = new Audio("./sounds/" + randomChosenColour + ".mp3");
   selectionSound.play();
 }
+
+// Adding EventListener to the event click on any of the button:
+$(".btn").click(function(){
+  var userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  $("#" + userChosenColour).addClass("pressed");
+  setTimeout(function(){$("#" + userChosenColour).removeClass("pressed")}, 100);
+  var clickingSound = new Audio("./sounds/" + userChosenColour + ".mp3");
+  clickingSound.play();
+})
