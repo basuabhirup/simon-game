034e0f67459ebc559bf68a11b3e874e860184516
basuabhirup <basu.abhirup@yahoo.com>
2021-07-13 13:41:31 +0530

    Created a function to select a random buttom + animate it + play sound.

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
+// Creating an empty array:
+gamePattern = [];
 
+// Creating an array to hold the button colors:
+var buttonColours = ["red","blue","green","yellow"];
+
+// Defining a function to select a random button + animate the button + play sound:
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(200);
+  var selectionSound = new Audio("./sounds/" + randomChosenColour + ".mp3");
+  selectionSound.play();
+}
